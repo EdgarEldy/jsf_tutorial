ebac82be1c8548eda33b857f2897bf726ffd037d
EdgarEldy <ed.eldy21@live.com>
2026-08-17 21:51:34 +0400

    docs: add class-level Javadoc to BaseDao

--- src/main/java/com/edgareldy/jsftutorial/dao/BaseDao.java
+++ src/main/java/com/edgareldy/jsftutorial/dao/BaseDao.java
@@ -3,6 +3,14 @@ package com.edgareldy.jsftutorial.dao;
 import java.io.Serializable;
 import java.util.List;
 
+/**
+ * Generic CRUD contract every entity-specific DAO of this project implements.
+ * <p>
+ * Created by Edgar Muhamyangabo on 8/17/26
+ * Author : Edgar Muhamyangabo
+ * Date : 8/17/26
+ * Project : jsf_tutorial
+ */
 public interface BaseDao<T, ID extends Serializable> {
 
     T findById(ID id);
