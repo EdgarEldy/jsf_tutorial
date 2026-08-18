fbcc04e746e32e0cba55ae71a4b7e5c240b6e79b
EdgarEldy <ed.eldy21@live.com>
2026-08-18 14:33:03 +0400

    feat(service): add ProductServiceImpl
    A thin pass-through to ProductDao, same shape as CategoryServiceImpl before this branch added its business rule.

---
+++ src/main/java/com/edgareldy/jsftutorial/service/impl/ProductServiceImpl.java
@@ -0,0 +1,45 @@
+package com.edgareldy.jsftutorial.service.impl;
+
+import com.edgareldy.jsftutorial.dao.ProductDao;
+import com.edgareldy.jsftutorial.entity.Category;
+import com.edgareldy.jsftutorial.entity.Product;
+import com.edgareldy.jsftutorial.service.ProductService;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import java.util.List;
+
+/**
+ * {@link ProductService} implementation, a thin pass-through to {@link ProductDao}.
+ * <p>
+ * Created by Edgar Muhamyangabo on 8/18/26
+ * Author : Edgar Muhamyangabo
+ * Date : 8/18/26
+ * Project : jsf_tutorial
+ */
+@ApplicationScoped
+public class ProductServiceImpl implements ProductService {
+
+    @Inject
+    private ProductDao productDao;
+
+    @Override
+    public List<Product> findAll() {
+        return productDao.findAll();
+    }
+
+    @Override
+    public List<Product> findByCategory(Category category) {
+        return productDao.findByCategory(category);
+    }
+
+    @Override
+    public Product save(Product product) {
+        return productDao.save(product);
+    }
+
+    @Override
+    public void delete(Product product) {
+        productDao.delete(product);
+    }
+}
