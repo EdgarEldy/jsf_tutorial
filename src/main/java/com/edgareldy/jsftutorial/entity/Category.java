8c514ac4c7955c2e541921a6fb53cacf76d22589
EdgarEldy <ed.eldy21@live.com>
2026-08-18 11:46:19 +0400

    feat(entity): add Category entity
    Maps the categories table (id, category_name varchar(255) not null). Products attach to a category via a foreign key added in feature/products, so there's no inverse @OneToMany collection here yet: adding it now would mean mapping a relation to an entity that doesn't exist.

---
+++ src/main/java/com/edgareldy/jsftutorial/entity/Category.java
@@ -0,0 +1,69 @@
+package com.edgareldy.jsftutorial.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ * A product category (e.g. "Electronics"). Products attach to exactly one
+ * category via {@code products.category_id}, added in feature/products.
+ * <p>
+ * Created by Edgar Muhamyangabo on 8/17/26
+ * Author : Edgar Muhamyangabo
+ * Date : 8/17/26
+ * Project : jsf_tutorial
+ */
+@Entity
+@Table(name = "categories")
+public class Category implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "category_name", length = 255, nullable = false)
+    private String categoryName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Category)) {
+            return false;
+        }
+        Category other = (Category) o;
+        return id != null && id.equals(other.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Category{id=" + id + ", categoryName='" + categoryName + "'}";
+    }
+}
