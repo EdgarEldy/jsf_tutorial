e4b33ebce8b81113eb50b2d3881ac4009ff4f023
EdgarEldy <ed.eldy21@live.com>
2026-08-18 14:32:29 +0400

    feat(entity): add Product entity
    Maps the products table: @ManyToOne to Category via category_id (not-null, every product belongs to exactly one category), product_name, unit_price.

---
+++ src/main/java/com/edgareldy/jsftutorial/entity/Product.java
@@ -0,0 +1,93 @@
+package com.edgareldy.jsftutorial.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ * A product belonging to exactly one {@link Category} via {@code category_id}.
+ * <p>
+ * Created by Edgar Muhamyangabo on 8/18/26
+ * Author : Edgar Muhamyangabo
+ * Date : 8/18/26
+ * Project : jsf_tutorial
+ */
+@Entity
+@Table(name = "products")
+public class Product implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+
+    @Column(name = "product_name", length = 255, nullable = false)
+    private String productName;
+
+    @Column(name = "unit_price", nullable = false)
+    private float unitPrice;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public float getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(float unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Product)) {
+            return false;
+        }
+        Product other = (Product) o;
+        return id != null && id.equals(other.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Product{id=" + id + ", productName='" + productName + "'}";
+    }
+}
